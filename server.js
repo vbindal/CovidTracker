f1dc8e75c8ebc127b5f9e9628cadebb1b3a8b000
Vishal Bindal <vbindal027@gmail.com>
2023-03-27 01:43:31 +0530

    changes made

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
 const express = require('express');
-const app = express();
+const app=  express();
 
-app.get('/', (req, res)=>{
+app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/index.html');
-    // res.sendFile(__dirname + '/style.css');
 })
-app.get('/style.css', (req, res) =>{
-    res.sendFile(__dirname + '/style.css')
-});
 
-app.listen(3000, ()=>{
-    console.log('server is listening to post 300, http://localhost:3000');
+app.get('/style.css',(req,res)=>{
+    res.sendFile(__dirname + '/style.css');
 })
+app.listen(3000,()=>{
+    console.log("server is running");
+})
+
+/*GET : TO GET THE DATA FROM THE SERVER/BACKEND,POST:FRONT END TO BACKEND ,
+PUT : UPDATION,PATCH : UPDATION ,DELETE : DELETE THE DATA */
+
+/*STATUS CODES : 200-400 : SUCCESS , 400-500 : FAILURE
+200: OKAY ,  201:USED WITH POST REQUEST, 
+404: NOT FOUND, 500: ERROR IN FETCHING THE DATA FROM THE SERVER  */
